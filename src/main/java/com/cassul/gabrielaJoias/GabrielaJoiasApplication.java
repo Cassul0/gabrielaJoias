d8046318d018a1061b13d53033a2e8a2934dc9ec
Gabriel Cassul <gabriel.c@unoesc.edu.br>
2024-03-06 21:48:23 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/cassul/gabrielaJoias/GabrielaJoiasApplication.java
@@ -0,0 +1,21 @@
+package com.cassul.gabrielaJoias;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GabrielaJoiasApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielaJoiasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	
+	}
+	
+	
+}
---
+++ src/test/java/com/cassul/gabriela_joias/GabrielaJoiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassul.gabriela_joias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrielaJoiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
