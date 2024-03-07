58b95fbedf01dfb0c5378ef2b160e542b45ffa23
Gabriel Cassul <gabriel.c@unoesc.edu.br>
2024-03-06 21:59:56 -0300

    Commit com aplicação subindo e acessando tela inicial

--- src/main/java/com/cassul/gabrielaJoias/web/controller/HomePageController.java
+++ src/main/java/com/cassul/gabrielaJoias/web/controller/HomePageController.java
@@ -2,12 +2,18 @@ package com.cassul.gabrielaJoias.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class HomePageController {
 
 	@GetMapping("/")
 	public String init() {
-		return "redirect:/home";
+		return "redirect:/homePage";
+	}
+	
+	@GetMapping("/homePage")
+	public String abrirPaginaInicial() {
+		return "homePage";
 	}
 }
