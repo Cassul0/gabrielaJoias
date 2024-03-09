ee23f0cf38aa575205ba0bd13179e636e139f175
Gabriel Cassul <gabriel.c@unoesc.edu.br>
2024-03-09 11:01:38 -0300

    Refs #5: incluido script para inicializar com registros no banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIO (NOME, EMAIL, SENHA) VALUES ('Teste1', 'emailTeste1@teste', 'teste1');
+INSERT INTO USUARIO (NOME, EMAIL, SENHA) VALUES ('Teste2', 'emailTeste2@teste', 'teste2');
+INSERT INTO USUARIO (NOME, EMAIL, SENHA) VALUES ('Teste3', 'emailTeste3@teste', 'teste3');
